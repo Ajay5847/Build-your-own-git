4b9899152d40ca79a5e3cae981e136b9793f9b58
Ajay Kumar M <ajaybunty91@gmail.com>
2024-10-12 23:39:42 +0530

    Working code

--- app/main.rb
+++ app/main.rb
@@ -5,55 +5,82 @@ require 'fileutils'
 # puts "Logs from your program will appear here!"
 
 # Uncomment this block to pass the first stage
-def create_tree(dir_path)
+# def create_tree(dir_path)
+#   entries = []
+#   Dir.entries(dir_path).sort.each do |path|
+#     next if ['.', '..', '.git'].include?(path)
+
+#     full_path = File.join(dir_path, path)
+#     is_dir = Dir.exist?(full_path)
+
+#     if is_dir
+#       mode = 40000
+#       sha1_hsh = create_tree(full_path)
+#     else
+#       mode = 100644  
+#       sha1_hsh = create_blob(full_path)
+#     end
+
+#     entries << "#{mode} #{path}\0#{sha1_hsh}"
+#   end
+
+#   full_entry_data = entries.join('')
+#   header = "tree #{full_entry_data.bytesize}\0"
+#   data_to_hsh = header + full_entry_data
+#   tree_sha1 = Digest::SHA1.hexdigest(data_to_hsh)
+#   store_object(tree_sha1, full_entry_data)
+#   tree_sha1
+# end
+
+# def create_blob(file_path)
+#   data = File.read(file_path)
+#   header = "blob #{data.bytesize}\0"
+#   data_to_hsh = header + data
+#   data_hex_hsh = Digest::SHA1.hexdigest(data_to_hsh)
+#   data_bin_hsh = Digest::SHA1.digest(data_to_hsh)
+#   store_object(data_hex_hsh, data)
+#   data_bin_hsh
+# end
+
+# def store_object(sha_hsh, data)
+#   compressed_data = Zlib::Deflate.deflate(data)
+#   dir_path = ".git/objects/#{sha_hsh[0..1]}"
+#   file_name = "#{sha_hsh[2..-1]}"
+
+#   FileUtils.mkdir_p(dir_path)
+#   File.write("#{dir_path}/#{file_name}", compressed_data)
+# end
+
+def process_dir(dirname)
   tree_entries = []
-  Dir.entries(dir_path).sort.each do |path|
-    next if ['.', '..', '.git'].include?(path)
-
-    full_path = File.join(dir_path, path)
-    is_dir = Dir.exist?(full_path)
-
-    if is_dir
-      mode = '40000'
-      sha1_hsh = create_tree(full_path)
-    else
-      mode = '100644'
-      sha1_hsh = create_blob(full_path)
-    end
-
-    tree_entries << "#{mode} #{path}\0#{sha1_hsh}"
+  Dir.entries(dirname).sort.each do |entry|
+    path = "#{dirname}/#{entry}"
+    is_dir = Dir.exist?(path)
+    next if ['.', '..', '.git'].include?(entry)
+    entry_mode = is_dir ? '40000' : '100644'
+    entry_content = if is_dir
+                      process_dir(path)
+                    else
+                      process_content(File.read(path), 'blob')
+                    end
+    tree_entries << "#{entry_mode} #{entry}\0#{entry_content[:binary_digest]}"
   end
-
-  full_entry_data = tree_entries.join('')
-  header = "tree #{full_entry_data.bytesize}\0"
-  data_to_hsh = header + full_entry_data
-  tree_sha1 = Digest::SHA1.hexdigest(data_to_hsh)
-
-  # Store the tree object using the full entry data
-  store_object(tree_sha1, data_to_hsh)
-  tree_sha1
+  process_content(tree_entries.join(''), 'tree')
 end
 
-def create_blob(file_path)
-  data = File.read(file_path)
-  header = "blob #{data.bytes.length}\0"  # Corrected to use `data.bytes.length`
-  data_to_hsh = header + data
-  data_hex_hsh = Digest::SHA1.hexdigest(data_to_hsh)
-  
-  # Only store the hex digest, not the binary digest as it's not used
-  store_object(data_hex_hsh, data_to_hsh)
-  data_hex_hsh  # Return the hex digest instead of binary
+def process_content(content, header_type)
+  entry_content = "#{header_type} #{content.bytes.length}\0#{content}"
+  hex_digest = Digest::SHA1.hexdigest(entry_content)
+  binary_digest = Digest::SHA1.digest(entry_content)
+  git_object_dir = File.join(Dir.pwd, '.git', 'objects', hex_digest[0..1])
+  Dir.mkdir(git_object_dir) unless Dir.exist?(git_object_dir)
+  git_object_path = File.join(git_object_dir, hex_digest[2..])
+  File.write(git_object_path, Zlib::Deflate.deflate(entry_content))
+  {
+    hex_digest: hex_digest,
+    binary_digest: binary_digest
+  }
 end
-
-def store_object(sha_hsh, data)
-  compressed_data = Zlib::Deflate.deflate(data)
-  dir_path = ".git/objects/#{sha_hsh[0..1]}"
-  file_name = "#{sha_hsh[2..-1]}"
-
-  FileUtils.mkdir_p(dir_path)
-  File.write("#{dir_path}/#{file_name}", compressed_data)
-end
-
 
 command = ARGV[0]
 case command
@@ -96,7 +123,7 @@ when "ls-tree"
     puts internal_data[-1]
   end
 when "write-tree"
-  tree_sha_hsh = create_tree(Dir.pwd)
+  tree_sha_hsh = process_dir(Dir.pwd)
   puts tree_sha_hsh
 else
   raise RuntimeError.new("Unknown command #{command}")
